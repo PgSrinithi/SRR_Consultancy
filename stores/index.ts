081bd574a6b886c6730489ddeace87b63f53dabd
Srinithi Govindaraj <srinithigovindaraj@admins-MacBook-Air.local>
2025-12-26 17:53:22 +0530

    Updated joblist section

--- stores/index.ts
+++ stores/index.ts
@@ -1,4 +1,4 @@
 export { industryStore, type Industry } from './IndustryStore';
-export { locationStore, type Location } from './LocationStore';
+export { locationStore } from './LocationStore';
 export { jobRoleStore, type JobRoles } from './JobRolesStore';
 export { jobPostingStore } from './JobPostingStore';
